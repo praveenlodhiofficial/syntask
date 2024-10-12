f75341d63d65d451bc6d007c21e848c31c15e5ae
praveenlodhi99 <praveenlodhi.official@gmail.com>
2024-10-13 00:19:47 +0530

    Add Account Section & Dropdown for Logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
  import { Navbar } from './components/Navbar'
+import { redirect } from 'next/navigation';
+import { auth } from './lib/auth';
 
-const HomePage = () => {
+const HomePage = async () => {
+  const session = await auth();
+
+
+  if(session?.user) {
+    return redirect('/dashboard');
+  }
   return (
-    <div>
+    <div className='max-w-8xl px-4 mx-auto sm:px-6 lg:px-8'>
       <Navbar/>
     </div>
   )
